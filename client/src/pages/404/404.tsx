2f1db57533de3e0c095b4b5dc83088997f467065
stefanbgh <stefanbgh.po@gmail.com>
2024-08-06 21:29:43 +0200

    update 404 page

--- client/src/pages/404/404.tsx
+++ client/src/pages/404/404.tsx
@@ -1,7 +1,33 @@
 import { FC } from "react";
 
-const NotFound: FC = () => {
-	return <div>NotFound</div>;
+import { NavLink, useNavigate } from "react-router-dom";
+import AppRoutes from "../../router/Routes";
+
+import mini_logo from "../../assets/mini-logo.webp";
+
+import "./404.less";
+
+const NotFound: FC = (): JSX.Element => {
+	const navigate = useNavigate();
+
+	const handleClick = () => {
+		navigate(AppRoutes.HOME);
+	};
+
+	return (
+		<div className="not-found">
+			<div className="message">
+				<NavLink to={AppRoutes.HOME} className="logo">
+					<img width={100} src={mini_logo} alt="logo" />
+				</NavLink>
+				<h1>Page Not Found!</h1>
+				<p>It seems the page you’re looking for doesn’t exist.</p>
+			</div>
+			<button className="btn btn-primary" onClick={handleClick}>
+				Go Back
+			</button>
+		</div>
+	);
 };
 
 export default NotFound;
